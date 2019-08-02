f119a931df62488fe3e0511edc8116f5eb1b8877
Tola's Terminal <sohbaker@gmail.com>
2019-08-02 11:43:29 +0100

    create fake server socket class

--- src/test/java/FakeServerSocket.java
+++ src/test/java/FakeServerSocket.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.net.ServerSocket;
 
-public class FakeServerSocket {
+public class FakeServerSocket extends ServerSocket {
+    FakeServerSocket(int port) throws IOException {
+    }
 }
