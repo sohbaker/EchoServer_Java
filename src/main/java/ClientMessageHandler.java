b7b5cac31d1e3d02eaae45d273edcdcc4287cee9
Tola's Terminal <sohbaker@gmail.com>
2019-08-02 12:34:56 +0100

    reads input until the client says 'bye'

--- src/main/java/ClientMessageHandler.java
+++ src/main/java/ClientMessageHandler.java
@@ -3,10 +3,27 @@ import java.io.*;
 public class ClientMessageHandler {
     private BufferedReader input;
     private PrintWriter output;
+    private String exitWord;
 
-    public ClientMessageHandler(BufferedReader input, PrintWriter output) {
+    public ClientMessageHandler(BufferedReader input, PrintWriter output, String exitWord) {
         this.input = input;
         this.output = output;
+        this.exitWord = exitWord;
+    }
+
+    public void run() {
+        while (true) {
+            String message = readInput();
+            System.out.println(message);
+            if (noMoreMessagesFromClient(message)) {
+                break;
+            }
+            sendOutput(message);
+        }
+    }
+
+    private boolean noMoreMessagesFromClient(String message) {
+       return message.equalsIgnoreCase(this.exitWord) || message == null;
     }
 
     public String readInput() {
--- src/test/java/ClientMessageHandlerTest.java
+++ src/test/java/ClientMessageHandlerTest.java
@@ -6,23 +6,36 @@ public class ClientMessageHandlerTest {
     private String fakeInput = "hello";
     private StringReader inputStream = new StringReader(fakeInput);
     private StringWriter outputStream = new StringWriter();
+    private String exitWord = "bye";
     private ClientMessageHandler messageHandler;
 
     @Before
     public void createHandler() {
         BufferedReader input = new BufferedReader(inputStream);
         PrintWriter output = new PrintWriter(outputStream);
-        messageHandler = new ClientMessageHandler(input, output);
+        messageHandler = new ClientMessageHandler(input, output, exitWord);
     }
 
     @Test
     public void receivesAnInput() {
-        assertEquals(messageHandler.readInput(), fakeInput);
+        assertEquals(fakeInput, messageHandler.readInput());
     }
 
     @Test
     public void outputsTheClientMessage() {
         messageHandler.sendOutput(fakeInput);
-        assertEquals(outputStream.toString(), fakeInput+"\n");
+        assertEquals(fakeInput+"\n", outputStream.toString());
+    }
+
+    @Test
+    public void readsInputUntilClientSaysBye() {
+        String multipleFakeInput = "hello\nhey\nhi\nbye";
+        BufferedReader input = new BufferedReader(new StringReader(multipleFakeInput));
+        StringWriter outputStream = new StringWriter();
+        PrintWriter output = new PrintWriter(outputStream);
+        messageHandler = new ClientMessageHandler(input, output, exitWord);
+
+        messageHandler.run();
+        assertEquals("hello\nhey\nhi\n", outputStream.toString());
     }
 }
