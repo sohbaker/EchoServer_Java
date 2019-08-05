033e62bbff557ba6b55e92498ce262952ea04e31
Tola's Terminal <sohbaker@gmail.com>
2019-08-05 13:47:54 +0100

    allow port and exit word to be passed from the command line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,8 @@ import java.net.ServerSocket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int port = 5000;
-        String exitWord = "bye";
+        int port = Integer.parseInt(args[0]);
+        String exitWord = args[1];
         ServerSocket server = new ServerSocket(port);
         MessageHandler messageHandler = new MessageHandler(new PrintWriter(System.out, true), port);
         messageHandler.confirmServerStarted();
