5c40fce0dfdcb0a912084c1f806bc4be6078c7ab
Tola's Terminal <sohbaker@gmail.com>
2019-08-05 13:10:51 +0100

    echos message from client. make method access private in EchoServer

--- src/test/java/EchoServerTest.java
+++ src/test/java/EchoServerTest.java
@@ -1,26 +1,25 @@
 import org.junit.*;
 import java.io.*;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
 
 public class EchoServerTest {
     private EchoServer server;
-    private int port = 3000;
-    private FakeServerSocket fakeServerSocket;
     private MessageHandler messageHandler;
-    private StringWriter outputStream = new StringWriter();
+    private String fakeInput = "hello";
+    private ByteArrayInputStream clientInputStream = new ByteArrayInputStream((fakeInput).getBytes());
+    private ByteArrayOutputStream clientOutputStream = new ByteArrayOutputStream();
     private String exitWord = "bye";
+    private FakeServerSocket fakeServerSocket;
 
     @Before
     public void setUp() throws IOException {
-        PrintWriter output = new PrintWriter(outputStream);
-        messageHandler = new MessageHandler(output, port);
-        fakeServerSocket = new FakeServerSocket();
+        int port = 3000;
+        PrintWriter serverOutput = new PrintWriter(new StringWriter());
+        messageHandler = new MessageHandler(serverOutput, port);
+        FakeClientSocket fakeClientSocket = new FakeClientSocket(clientInputStream, clientOutputStream);
+        fakeServerSocket = new FakeServerSocket(fakeClientSocket);
         server = new EchoServer(messageHandler, fakeServerSocket, exitWord);
-    }
-
-    @After
-    public void closeServerSocket() {
-        server.stop();
     }
 
     @Test
@@ -31,12 +30,7 @@ public class EchoServerTest {
 
     @Test
     public void echoesAMessageFromTheClientBackToTheClient() {
-    }
-
-    @Test
-    public void receivesARequestFromAClientSocket() {
-        MockClientSocket mockClientSocket = new MockClientSocket();
-        String request = server.acceptRequestFromAClient(mockClientSocket);
-        assertEquals("GET / HTTP/1.1", request);
+        server.listen();
+        assertThat(clientOutputStream.toString(), containsString("hello"));
     }
 }
--- src/test/java/FakeClientSocket.java
+++ src/test/java/FakeClientSocket.java
@@ -1,4 +1,22 @@
+import java.io.*;
 import java.net.Socket;
 
 public class FakeClientSocket extends Socket {
+    private ByteArrayInputStream inputStream;
+    private ByteArrayOutputStream outputStream;
+
+    public FakeClientSocket(ByteArrayInputStream inputStream, ByteArrayOutputStream outputStream) {
+        this.inputStream = inputStream;
+        this.outputStream = outputStream;
+    }
+
+    @Override
+    public ByteArrayInputStream getInputStream() {
+        return this.inputStream;
+    }
+
+    @Override
+    public ByteArrayOutputStream getOutputStream() {
+        return this.outputStream;
+    }
 }
