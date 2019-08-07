7bbe135590e96cd24a8a5e2eee0f95af16d0dd2d
Tola's Terminal <sohbaker@gmail.com>
2019-08-07 16:12:28 +0100

    pass executor as dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.ServerSocket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -8,8 +10,9 @@ public class Main {
         ServerSocket server = new ServerSocket(port);
         MessageHandler messageHandler = new MessageHandler(new PrintWriter(System.out, true));
         messageHandler.confirmServerStarted(port);
+        Executor executor = Executors.newCachedThreadPool();
 
-        EchoServer echoServer = new EchoServer(messageHandler, server, exitWord);
+        EchoServer echoServer = new EchoServer(messageHandler, server, exitWord, executor);
         echoServer.start();
     }
 }
