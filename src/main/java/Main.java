7e9c8d098f9b8b6913123d6d389fc51d6e596982
Tola's Terminal <sohbaker@gmail.com>
2019-08-07 17:43:50 +0100

    reorder assignments and declarations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,19 @@
 import java.io.*;
 import java.net.ServerSocket;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         int port = Integer.parseInt(args[0]);
         String exitWord = args[1];
+
         ServerSocket server = new ServerSocket(port);
         MessageHandler messageHandler = new MessageHandler(new PrintWriter(System.out, true));
-        messageHandler.confirmServerStarted(port);
         Executor executor = Executors.newCachedThreadPool();
 
-        EchoServer echoServer = new EchoServer(messageHandler, server, exitWord, executor);
+        messageHandler.confirmServerStarted(port);
+
+        EchoServer echoServer = new EchoServer(server, messageHandler, executor, exitWord);
         echoServer.start();
     }
 }
